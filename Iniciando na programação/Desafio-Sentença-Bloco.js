3d9b6fe133c92b7b768145e04867477d4ede3586
irineu <irineu.joel01@gmail.com>
2022-03-29 14:57:30 -0300

    Desafio sentenças e blocos

---
+++ Iniciando na programação/Desafio-Sentença-Bloco.js
@@ -0,0 +1,16 @@
+// Exiba os numeros impares entre 1 e 10 e os pares, organizando cada categoria em blocos de codigo
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(7);
+    console.log(10);
+}
