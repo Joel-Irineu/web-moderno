0a5eee5d08001e0c2d88fe4ed3f05cac9ba51074
irineu <irineu.joel01@gmail.com>
2022-03-29 14:51:44 -0300

    blocos de código

---
+++ Iniciando na programação/Blocos.js
@@ -0,0 +1,10 @@
+// Em JS um bloco é definido por um par de chaves {}
+{
+    console.log('Este é um bloco');
+    {
+        console.log('Este é um bloco interno');
+    }
+}
+{
+    console.log('Este é outro bloco de codigo');
+}
