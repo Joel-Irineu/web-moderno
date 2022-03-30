0a1f4ba7657feff5a629304034ed84a25d8113ea
irineu <irineu.joel01@gmail.com>
2022-03-30 16:02:19 -0300

    Trabalhando com dados #03

---
+++ Iniciando na programação/dados03.js
@@ -0,0 +1,11 @@
+let idade = 31;
+console.log(typeof idade);
+
+let salario = 1234.5;
+console.log(typeof salario);
+
+let life = true;
+console.log(typeof life);
+
+let nome = "Maria";
+console.log(typeof nome);
