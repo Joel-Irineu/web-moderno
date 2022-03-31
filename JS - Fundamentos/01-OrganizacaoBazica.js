f2608c65f30c57d8a517cfc8ea12765398538a30
irineu <irineu.joel01@gmail.com>
2022-03-31 14:12:04 -0300

    Organização basica de código js

---
+++ JS - Fundamentos/01-OrganizacaoBazica.js
@@ -0,0 +1,10 @@
+console.log('Sentença de código');
+
+{
+    console.log('Bloco de código');
+    {
+        console.log('outro bloco de código');
+    }
+}
+
+console.log('outra sentença de código');
