330358dc3a1c7f323112f12f0668d08c34d08d09
irineu <irineu.joel01@gmail.com>
2022-03-31 14:24:52 -0300

    O básico de var, let e const

---
+++ JS - Fundamentos/03-varLetConst.js
@@ -0,0 +1,9 @@
+// = é utilizado para atribuir valores a uma variável
+
+// var é utilizado para declarar uma variável
+var a = 3;
+// let é utilizado para declarar uma variável
+let b = 4;
+// const é utilizado para declarar uma constante
+const c = a + b;
+console.log(c);
